54161b2138d281076290d40a6bd22692ccdf5a3b
Maxim Zverev <max@rhomobile.com>
2011-08-04 11:05:41 -0700

    Original update

---
+++ RhoconnectNET/RhoconnectNET/Controllers/RhoconnectCRUD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Mvc;
+using System.Collections;
+using System.IO;
+using System.Diagnostics;
+
+namespace RhoconnectNET.Controllers
+{
+    public interface RhoconnectCRUD
+    {
+        JsonResult rhoconnect_query_objects();
+        String rhoconnect_partition();
+        String rhoconnect_id_field();
+        ActionResult rhoconnect_create(String objJson);
+        ActionResult rhoconnect_update(Dictionary<string, object> changes);
+        ActionResult rhoconnect_delete(Object objId);
+        IModelBinder rhoconnect_model_binder(Type parameterType);
+    }
+}
