b70c3106b4c6d7433465f388b01f404a68ad68e2
abhineetagarwal <pwmj34@zebra.com>
2016-03-04 16:01:02 +0530

    Modified copyright issue

--- RhoconnectNET/RhoconnectNET/Properties/AssemblyInfo.cs
+++ RhoconnectNET/RhoconnectNET/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RhoconnectNET")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RhoconnectNET")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
